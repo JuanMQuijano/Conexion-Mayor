985aad170755c9348780e0e0e1a7fb588ca843a8
Juan Q <jmquijano@unimayor.edu.co>
2023-05-07 03:01:03 -0500

    Build file

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,11 +34,10 @@ class PostController extends Controller
         $imagen_servidor->fit(1000, 1000);
 
         $imagen_path = public_path('uploads') . '/' . $nombre_imagen;
-        $imagen_servidor->save($imagen_path);
-
         if (!file_exists($imagen_path)) {
             mkdir($imagen_path, 0777, true);
         }
+        $imagen_servidor->save($imagen_path);
 
         $url = Str::uuid();
         $request->request->add(['url' => Str::slug($url)]);
